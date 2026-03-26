43572b15999510bb78b788c094c59e97c4a667fe
Emily Britto <themilygomes@gmail.com>
2026-03-26 10:54:04 -0300

    feat: implement RMSE heuristic

--- block_world_problem.py
+++ block_world_problem.py
@@ -69,8 +69,9 @@ class BlockWorld(State):
         return 1
     
     def h(self):
-        if self.heuristic == "":
-            return 0
+        if self.heuristic == "rmse":
+            return self._heuristica_rmse()
+        return 0
     
     def env(self):
         return self.state
@@ -83,6 +84,43 @@ class BlockWorld(State):
             Objetivo:
             {self.format_state(self.goal)}
         """
+    
+    
+    def _heuristica_rmse(self):
+        '''
+        Heurística 1: RMSE
+
+        Cada bloco tem uma posição (coluna, altura) no estado atual e no objetivo.
+        Calcula o erro quadrático entre as posições de cada bloco e tira a média,
+        aplicando raiz quadrada no final:
+        RMSE = sqrt( (1/n) * sum( (delta_coluna)^2 + (delta_altura)^2 ) )
+        Blocos ausentes em um dos estados recebem posição padrão (0, 0).
+        '''
+        
+        def get_posicoes(state):
+            # dicionário pra armazenar as posições dos blocos
+            positions = {}
+            for i, stack in enumerate(state):
+                for j, block in enumerate(stack):
+                    positions[block] = (i, j)
+            return positions
+        
+        pos_atual = get_posicoes(self.state)
+        pos_goal = get_posicoes(self.goal)
+        
+        total_blocos = set(pos_atual.keys()) | set(pos_goal.keys()) # união dos blocos do estado atual + objetivo
+        n = len(total_blocos)
+        
+        if n == 0:
+            return 0
+        erros = []
+        for block in total_blocos:
+            pos_i, pos_j = pos_atual.get(block, (0,0))
+            meta_i, meta_j = pos_goal.get(block, (0,0))
+            erros.append((pos_i - meta_i)**2 + (pos_j - meta_j)**2)
+        
+        return (sum(erros)/n)**0.5
+        
     
     # @staticmethod
     # def my_show_path(result):
